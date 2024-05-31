9c8b8e4038c4b137019405d449a17a7e9900aace
ansftnmz <anisfatini15@gmail.com>
2024-06-01 03:02:56 +0800

    Initial commit

---
+++ test.php
@@ -0,0 +1,104 @@
+<?php
+include 'dbconnect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form inputs
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = sha1($_POST['password']);
+    $phoneno = $_POST['phoneno'];
+    $address = $_POST['address'];
+
+    // Prepare and bind
+    $sqlreguser = "INSERT INTO `tbl_user`(`user_name`, `user_email`, `user_pass`,`user_phone`, `user_address`) 
+    VALUES ('$name','$email ','$password','$phoneno','$address')";
+
+    echo $sqlreguser;
+}
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>Register- iClinic</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="login-page">
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-md-center h-100">
+				<div class="card-wrapper">
+					<div class="brand">
+						<img src="img/logo.jpg" alt="register page">
+					</div>
+					<div class="card fat">
+						<div class="card-body">
+							<h4 class="card-title">Register</h4>
+							<form method="POST" class="my-login-validation" novalidate="">
+								<div class="form-group">
+									<label for="name">Name</label>
+									<input id="name" type="text" class="form-control" name="name" required autofocus>
+									<div class="invalid-feedback">
+										What's your name?
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="email">E-Mail Address</label>
+									<input id="email" type="email" class="form-control" name="email" required>
+									<div class="invalid-feedback">
+										Your email is invalid
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="password">Password</label>
+									<input id="password" type="password" class="form-control" name="password" required data-eye>
+									<div class="invalid-feedback">
+										Password is required
+									</div>
+								</div>
+
+                                <div class="form-group">
+									<label for="phoneno">Phone Number</label>
+									<input id="phoneno" type="phoneno" class="form-control" name="phoneno" required data-eye>
+									<div class="invalid-feedback">
+                                    Phone Number is required
+									</div>
+								</div>
+
+                                <div class="form-group">
+									<label for="address">Address</label>
+									<textarea id="address" type="address" class="form-control" name="address" required data-eye></textarea>
+									<div class="invalid-feedback">
+                                    Address is required
+									</div>
+								</div>
+
+								<div class="form-group m-0">
+									<button type="submit" class="btn btn-primary btn-block">
+										Register
+									</button>
+								</div>
+								<div class="mt-4 text-center">
+									Already have an account? <a href="login.php">Login</a>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="footer">
+						Copyright &copy; 2017 &mdash; iClinic Sdn. Bhd.
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="js/my-login.js"></script>
+</body>
+</html>
